996148679d7b941623ba1b06bcf3c041b3b4ec11
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-31 19:41:51 -0400

    Corrected promedio method and added its calling

--- arreglo_notas.rb
+++ arreglo_notas.rb
@@ -4,14 +4,19 @@ def promedio(arr)
     suma = 0
     arr.each do |e|
         if e == "N.A"
-            new_arr.push(2.0)
+            new_arr.push(2)
         else
             new_arr.push(e)
         end
     end
     new_arr.each do |e|
-        prom += e
+        suma += e
     end
-    return prom/(arr.count)
+    return suma/(arr.count)
 end
 
+#Calling the method
+notas = [5, 7, 1, 3, 5, 8, 9, 'N.A', 'N.A', 3]
+promedio_notas = promedio(notas)
+
+print "#{promedio_notas}\n"
